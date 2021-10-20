1471234cfef58e2a595aafd4f8857acf4fd686d8
Ayush Kumar Singh <45496026+Ayush-KS@users.noreply.github.com>
2021-10-20 12:32:38 +0530

    add algorithm explanation

--- graph/bellman-ford.py
+++ graph/bellman-ford.py
@@ -1,6 +1,17 @@
 # The Bellman–Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the other vertices in a weighted digraph. It is slower than Dijkstra's algorithm for the same problem, but more versatile, as it is capable of handling graphs in which some of the edge weights are negative numbers. The algorithm was first proposed by Alfonso Shimbel (1955), but is instead named after Richard Bellman and Lester Ford Jr., who published it in 1958 and 1956, respectively.
 # Author: Ayush-KS
 
+
+# Explanation-
+
+# We will create an array of distances d[0…n−1], which after execution of the algorithm will contain the answer to the problem. In the beginning we fill it as follows: d[v]=0, and all other elements d[] equal to infinity ∞.
+
+# The algorithm consists of several phases. Each phase scans through all edges of the graph, and the algorithm tries to produce relaxation along each edge (a,b) having weight c. Relaxation along the edges is an attempt to improve the value d[b] using value d[a]+c. In fact, it means that we are trying to improve the answer for this vertex using edge (a,b) and current response for vertex a.
+
+# It is claimed that n−1 phases of the algorithm are sufficient to correctly calculate the lengths of all shortest paths in the graph (again, we believe that the cycles of negative weight do not exist). For unreachable vertices the distance d[] will remain equal to infinity ∞.
+
+
+
 # number of nodes in the graph
 print('Enter number of edges in the graph:- ')
 n = int(input())
