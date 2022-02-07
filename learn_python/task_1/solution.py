44b0edc402db37c263f223f0d69a91ca6628c224
Himanshu Khanna <himanshu.khanna289@gmail.com>
2022-02-07 11:04:58 +0530

    task 1 completed

--- learn_python/task_1/solution.py
+++ learn_python/task_1/solution.py
@@ -1,41 +1,30 @@
 try:
     file = open("mock_data.csv", "r")
 except:
-    print ("An error occurred")
+    print ("File not found")
 ids = []
 thisdict = {}
 ip = ()
 i = []
 x = []
-#print(file)
 index = 0
 for line in file:
     if index != 0:
         line = line.strip()
         row = line.split(",")
-        # if row[1][0] in ['A','S']:
         if row[1][0] == 'A' or row[1][0] == 'S': #step 1
-            # print(row[0] + "->" + row[1])
-            ids += [row[0]] # printing result
-            
+            ids += [row[0]]           
         if row[4] == "Female": #step 2
            thisdict[row[0]] = row[1] # thisdict[key] = value # printing result
-           
            i += [row[0]] #step 4
            s = set(i)
-           
            def id(): #step 5
                global x
                x += [row[5]]
                return x
            id() # printing result
-           
         if '7' in row[5] or '8' in row[5] or '9' in row[5]: #step 3
             ip += (row[0],) # printing result
-             
-    # if index == 50:
-    #     break
-    # print (x)
     index += 1
 print("List of ids whose corresponding first_name starts with either 'A' or 'S' are ->\r\n",  ids) #Create a list of ids whose corresponding first_name starts with either 'A' or 'S'
 print("Dictionary containing the key = 'id' and value = 'first_name' of all the Females ->\r\n",  thisdict) # Create a dictionary containing the key = 'id' and value = 'first_name' of all the Females.
