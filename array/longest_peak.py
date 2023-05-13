ec7c7d522c68333da3eb7c459eb5a13bd77c58d2
Munish Sharma <munish7771@gmail.com>
2023-05-14 00:56:16 +0530

    Create longest_peak.py

---
+++ array/longest_peak.py
@@ -0,0 +1,7 @@
+# Write a function that takes in an array of integers and returns the length of the longest peak.
+# peak = adjacent integers that are strictly increasing until they reach a peak after that they become strictly decreasing. At minimum three integers are required to form a peak.
+
+# eg - 1,4,10,2 form a peak, but 4,0,10 dont
+# 1,2,2,0 also do not form a peak
+# 1,2,3 do not form a peak because there is no decreasing integer after 3.
+
